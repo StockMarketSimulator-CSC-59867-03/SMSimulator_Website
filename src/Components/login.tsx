65332b76fdeff29e4b25d615c5c6a19578b0697f
karnowsky1 <kestime9015@bths.edu>
2020-02-19 07:04:24 -0500

    adding login and signup components

---
+++ src/Components/login.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+type LogInProps = {};
+type LogInState = {
+  email:          string,
+  password:       string,
+};
+
+class LogIn extends React.Component<LogInProps, LogInState> {
+  constructor(props : any){
+    super(props);
+    this.state = {
+      email:          "",
+      password:       "",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+handleChange(event : any) {
+    this.setState({
+        ...this.state,
+        [event.target.name]: event.target.value,
+    })
+}
+
+handleSubmit(event : any) {
+    alert("Order submitted");
+    event.preventDefault();
+}
+
+render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label>Email <input type="email" name="userEmail" onChange={this.handleChange}/></label>
+        <br/>
+        <label>Password <input type="password" name="userPassword" onChange={this.handleChange}/></label>
+        <br/>
+        <br/>
+        <input type="submit" value="Create"/>
+    </form>
+    );
+}
+
+}
+
+export default LogIn;
---
+++ src/Components/signup.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+type SignUpProps = {};
+type SignUpState = {
+  email:          string,
+  password:       string,
+  username:       string,
+};
+
+class SignUp extends React.Component<SignUpProps, SignUpState> {
+  
+  constructor(props : any){
+    super(props);
+    this.state = {
+        email:          "",
+        password:       "",
+        username:       "",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleChange(event : any) {
+    let value;
+    event.target.value <= 0 ? value = 0 : value = event.target.value;
+    this.setState({
+        ...this.state,
+        [event.target.name]: value,
+    })
+}
+
+handleSubmit(event : any) {
+    alert("Order submitted");
+    event.preventDefault();
+}
+
+render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label>Email <input type="email" name="userEmail" onChange={this.handleChange}/></label>
+        <br/>
+        <label>Password <input type="password" name="userPassword" onChange={this.handleChange}/></label>
+        <br/>
+        <br/>
+        <input type="submit" value="Sign Up"/>
+    </form>
+    );
+}
+
+}
+
+export default SignUp;
