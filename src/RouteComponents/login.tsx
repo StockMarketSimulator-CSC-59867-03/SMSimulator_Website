bf59983f95ec3bf4c80611d1c43314f7033857fe
coollama1 <lamemarvin@gmail.com>
2020-02-28 14:05:10 -0500

    adding login and setup files

--- src/RouteComponents/login.tsx
+++ src/RouteComponents/login.tsx
@@ -1,17 +1,77 @@
-import { Link } from 'react-router-dom';
-
-
 import React from 'react';
+import { Link } from 'react-router-dom';
+import firebase from 'firebase';
 
-class Login extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Log in page</h1>
-                <Link to="../Hpage/home"> Back to home </Link>
-            </div>
-        );
+type LogInProps = {};
+type LogInState = {
+  email:          string,
+  password:       string,
+};
+
+class LogIn extends React.Component<LogInProps, LogInState> {
+  constructor(props : any){
+    super(props);
+    this.state = {
+      email:          "",
+      password:       "",
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+handleChange(event : any) {
+    this.setState({
+        ...this.state,
+        [event.target.name]: event.target.value,
+    })
 }
 
-export default Login;
+handleSubmit(event : any) {
+    alert("Succesfully Loggeed In!");
+    event.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password).catch(
+      function(error){
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      }
+    );
+}
+
+render() {
+    return (
+      <div> 
+          <h1>Login Page</h1>
+          <form onSubmit={this.handleSubmit}>
+            <br/>
+            <label>Email <input type="email" name="email" onChange={this.handleChange}/></label>
+            <br/>
+            <label>Password <input type="password" name="password" onChange={this.handleChange}/></label>
+            <br/>
+            <br/>
+            <input type="submit" value="Create"/>
+            <br/>
+          </form>
+          <Link to="/"> Back to home</Link>
+      </div>
+
+    );
+}
+
+}
+/*
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in. Username can be retrieved here
+    const displayName = user.displayName;
+    const email = user.email;
+    const uid = user.uid;
+    
+  } else {
+    // User is signed out.
+    
+  }
+});
+*/
+export default LogIn;
