2e6adf438c264de62ccbb6122d222a010948bba5
nmaswood98 <nmaswood98@gmail.com>
2020-03-19 14:55:51 -0400

    SMS-75 Created the notification system adding in instant full screen dialog notifications and snackbar info notifications

--- src/Components/NotificationComponent/NotificationComponent.tsx
+++ src/Components/NotificationComponent/NotificationComponent.tsx
@@ -6,6 +6,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Notification } from '../../DataModels/notification';
+import { useSelector, useDispatch } from 'react-redux';
+import { replaceNotifications } from '../../redux/actions';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+
+
+function Alert(props: AlertProps) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
 
 function NotificationComponent(props: any){
@@ -16,59 +25,90 @@ function NotificationComponent(props: any){
         body:""
     };
 
-    const [notifications , setNotifications] = React.useState([]);
+    let defaultSnackBar: Notification = {
+        type:"SNACKINFO",
+        title:"",
+        body:""
+    };
+
+    let dispatch = useDispatch();
+    let notifications = useSelector((state: any) => state.notifications);
+
 
     const [instant, setInstant] = React.useState({open: false, notification: defaultInstant});
-
+    const [snackBar, setSnackBar] = React.useState({open: false, notification: defaultSnackBar});
   
-    const handleClose = () => {
+    const handleInstantClose = () => {
         setInstant({
             open: false,
             notification: defaultInstant
         });
     };
 
-    if(notifications.length > 0 && instant.open == false){
-        let instantNotification = notifications[0];
-        let filteredArray = notifications.filter(item => item !== instantNotification);
-
-        setInstant({
-            open: true,
-            notification: instantNotification
+    const handleSnackClose = () => {
+        setSnackBar({
+            open: false,
+            notification: defaultSnackBar
         });
+    };
 
-        setNotifications(filteredArray);
+    if(notifications.length > 0 && instant.open == false && snackBar.open == false){
+        let notif = notifications[0];
+        let filteredArray = notifications.filter((item: any) => item !== notif);
+
+        switch(notif.type) {
+            case "INSTANT":
+                setInstant({
+                    open: true,
+                    notification: notif
+                });
+              break;
+            case "SNACKINFO":
+                setSnackBar({
+                    open: true,
+                    notification: notif
+                });
+              break;
+            default:
+              // code block
+          }
+
+        dispatch(replaceNotifications(filteredArray));
     } 
 
     console.log("Rendering Notification");
 
-   return  (
-        <div >
+   return (
+     <div>
+       <Dialog
+         open={instant.open}
+         onClose={handleInstantClose}
+         aria-labelledby="alert-dialog-title"
+         aria-describedby="alert-dialog-description"
+       >
+         <DialogTitle id="alert-dialog-title">
+           {instant.notification.title}
+         </DialogTitle>
+         <DialogContent>
+           <DialogContentText id="alert-dialog-description">
+             {instant.notification.body}
+           </DialogContentText>
+         </DialogContent>
+         <DialogActions>
+           <Button onClick={handleInstantClose} color="primary" autoFocus>
+             Ok
+           </Button>
+         </DialogActions>
+       </Dialog>
 
-      <Dialog
-        open={instant.open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{instant.notification.title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {instant.notification.body}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Ok
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <div>
-
-            {props.children}
-            </div>
-         </div>
-     );
+       <Snackbar open={snackBar.open} autoHideDuration={6000} onClose={handleSnackClose}>
+         <Alert onClose={handleSnackClose} severity="info">
+           {snackBar.notification.title}
+         </Alert>
+       </Snackbar>
+       <div>{props.children}</div>
+     </div>
+   );
 }
 
 
