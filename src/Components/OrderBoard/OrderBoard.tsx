6991dc43a842401d7b7af75d1283ddf7f9b49454
coollama1 <lamemarvin@gmail.com>
2020-05-13 15:31:46 -0400

    adjusting OrderBoard table dimentsions

---
+++ src/Components/OrderBoard/OrderBoard.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { withStyles, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import firebase from 'firebase';
+import { useDispatch, useSelector } from "react-redux";
+import { Order } from '../../DataModels/order.model';
+import { Button } from '@material-ui/core';
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+      width: '25%'
+    },
+    body: {
+      fontSize: 14,
+      width: '25%'
+    },
+  }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.background.default,
+      },
+    },
+  }),
+)(TableRow);
+
+function createData(orderID:any,stockSymbol: any,stockPrice: any,stockQuantity: any) {
+    return { orderID : orderID, stockSymbol: stockSymbol,stockPrice: stockPrice,stockQuantity: stockQuantity};
+}
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 200,
+        minHeight: 50,
+    },
+});
+
+function addDocToArray(doc : any,orderArray : object[]){
+    const docData = doc.data();
+    if(docData!= null){
+        const orderID = doc.id;
+        const price = docData.price;
+        const quantity = docData.quantity;
+        const stockSymbol = docData.stock;
+        orderArray.push(createData(orderID,stockSymbol,price,quantity))
+    }
+    
+}
+
+  export function OrderBoard(){
+    const classes = useStyles();
+    const db = firebase.firestore();
+    let buyOrders : object[] = [], sellOrders : object[] = [];
+    const currentUser = useSelector((state: any)=> state.currentUserData);
+    const currentSessionID = useSelector((state: any) => state.sessionData.id);
+    const currentUserID = currentUser.id;
+    buyOrders.push(createData(1,'AMZN',15.55,5));
+
+    db.collection("BuyOrders")
+    .where('sessionID','==',currentSessionID)
+    .where('user', '==', currentUserID)
+    .get()
+    .then((querySnapshot : any)=>{
+        querySnapshot.forEach((doc : any) =>{
+            addDocToArray(doc,buyOrders);
+        });
+    });
+    
+    db.collection("SellOrders")
+    .where('sessionID','==',currentSessionID)
+    .where('user', '==', currentUserID)
+    .get()
+    .then((querySnapshot : any)=>{
+        querySnapshot.forEach((doc : any) =>{
+            addDocToArray(doc,sellOrders);
+        });
+    });
+
+    return (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align="center">Stock</StyledTableCell>
+                <StyledTableCell align="center">Price</StyledTableCell>
+                <StyledTableCell align="center">Quantity</StyledTableCell>
+                <StyledTableCell align="center"></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {buyOrders.map((buyOrder : any) => (
+                <StyledTableRow key={buyOrder.name}>
+                  <StyledTableCell component="th" scope="row" align="center">
+                    {buyOrder.stockSymbol}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{buyOrder.stockPrice}</StyledTableCell>
+                  <StyledTableCell align="center">{buyOrder.stockQuantity}</StyledTableCell>
+                  <StyledTableCell align="center"><Button disableElevation color="secondary" variant="contained">Cancel</Button></StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+  }
+
+  export default OrderBoard;
